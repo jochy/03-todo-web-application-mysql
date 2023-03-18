2ed0591be68cb104c220e6f33e239b99232a9c40
Alex Dubois <ad.alexandre.dubois@gmail.com>
2023-03-18 15:10:42 +0100

    Fix multipe issues

--- src/main/java/com/in28minutes/springboot/web/security/SecurityConfiguration.java
+++ src/main/java/com/in28minutes/springboot/web/security/SecurityConfiguration.java
@@ -8,11 +8,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
-	//Create User - in28Minutes/dummy
+
 	@Autowired
     public void configureGlobalSecurity(AuthenticationManagerBuilder auth)
             throws Exception {
-        auth.inMemoryAuthentication().withUser("atingupta2005").password("{noop}dummy")
+        auth.inMemoryAuthentication().withUser("root").password("{noop}root")
                 .roles("USER", "ADMIN");
     }
 	
